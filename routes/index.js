4522fa93a2e4917d53db7ed1e626b918dfe8091a
ymzk-jp <ymzk@gmail.com>
2018-10-11 14:13:04 +0900

    copy and paste

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,34 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send(process.env);
 });
 
-router.get('/tag', function(req, res, next) {
-  res.send('tag');
+router.get('/tag', function (req, res, next) {
+  var async = require('async');
+  var SensorTag = require('sensortag');
+  SensorTag.discover(function (sensorTag) {
+    sensorTag.on('disconnect', function () {
+      process.exit(0);
+    });
+    async.series([function (callback) {
+      sensorTag.connectAndSetUp(callback);
+    }, function (callback) {
+      sensorTag.enableIrTemperature(callback);
+    }, function (callback) {
+      setTimeout(callback, 2000);
+    }, function (callback) {
+      sensorTag.readIrTemperature(function (error, objectTemperature, ambientTemperature) {
+        console.log("temp: " + ambientTemperature);
+        callback();
+      });
+    }, function (callback) {
+      sensorTag.disableIrTemperature(callback);
+    }, function (callback) {
+      sensorTag.disconnect(callback);
+    }]);
+  });
 });
 
 module.exports = router;
